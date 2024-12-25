9977ad669b45dea57e13a1583bef097f72d9e17a
Juan Cruz Od <johnmanito3@gmail.com>
2024-12-25 12:57:24 -0500

    optimizacion0.1

--- src/ScrollToTopButton.tsx
+++ src/ScrollToTopButton.tsx
@@ -4,15 +4,15 @@ import { ArrowUpward } from '@material-ui/icons';
 
 export default function ScrollToTopButton() {  
     const [visible, setVisible] = useState(false);   
-    const [opaciton, setOpaciton] = useState(0);   
+    // const [opaciton, setOpaciton] = useState(0);   
 
     const handleScroll = () => {  
         if (window.scrollY > 300) {  
             setVisible(true); 
-            setOpaciton(1) 
+            // setOpaciton(1) 
         } else {  
             setVisible(false);  
-            setOpaciton(0) 
+            // setOpaciton(0) 
         }  
     };  
 
@@ -35,22 +35,21 @@ export default function ScrollToTopButton() {
             variant="contained"   
             color="primary"  
             aria-label="Scroll to top"  
-            startIcon={<ArrowUpward className="bounce-icon" style={{ fontSize: '17px', marginLeft: "11px" }} />}  
+            startIcon={<ArrowUpward className="bounce-icon" style={{ fontSize: '25px', marginLeft: "0px" }} />}  
             onClick={scrollToTop}  
             style={{  
                 display: visible ? 'block' : 'none',  
                 position: 'fixed',  
                 bottom: '40px',  
-                right: '20px',   
-                borderRadius: '50%',   
+                right: '20px',
+                borderRadius: '30%',   
                 zIndex: 1000, // Asegúrate de que esté por encima de otros elementos   
-                opacity: visible ? opaciton : 0,  
+                opacity: 0.8,  
                 transition: 'opacity 10s ease', // Transición suave  
-                fontSize: '9px',  
-                padding: '14px 0px', // Ajusta el padding según sea necesario  
+                fontSize: '0px',  
+                padding: '9px 0px 9px 9px ', // Ajusta el padding según sea necesario  
             }}   
         >  
-            SUBIR  
         </Button>   
     );  
 }
